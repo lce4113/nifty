01149783d0228e777696bfe9c46b73fe38c01d9c
Om Mahesh <2om.mahesh@gmail.com>
2020-11-14 22:23:29 -0800

    Bars are proportional.

--- Bar Chart Racer/JS/frames.js
+++ Bar Chart Racer/JS/frames.js
@@ -5,11 +5,12 @@ function setup(data) {
 
 function draw(stamp) {
   $("#graph").empty();
+  const max = stamp.bars[0].value
   for (const bar of stamp.bars) {
     if (bar.value > 0) {
       $("#graph").append(`
 <div class="bar-wrapper">
-  <div class="bar">
+  <div class="bar" style="width: ${bar.value / max * 100}%;">
     <div class="name">${bar.name}</div>
   </div>
   <div class="value">${bar.value}</div>
